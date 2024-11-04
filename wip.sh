5225566b8aec4e93e7305d1b7d8495ff3f39c96c
Carlos Cueto <crcueto25@gmail.com>
2024-11-04 20:51:50 +0200

    add wip commands

---
+++ wip.sh
@@ -0,0 +1,16 @@
+docker build --no-cache \
+-f docker/Dockerfile \
+-t ouster_lidar_image:latest \
+--target full \
+--build-arg AMENT_WORKSPACE_DIR=/ament_ws --build-arg USERNAME=devuser --build-arg USER_UID=1000 --build-arg USER_GID=1000 .
+
+
+docker save -o ouster_lidar_image.tar ouster_lidar_image:latest
+
+docker load -i ouster_lidar_image.tar
+
+
+
+docker-compose up -d
+
+docker-compose --progress plain -f ./docker/docker-compose.yml up
