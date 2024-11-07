c936e15246f0d7bf081ad277ea434e96c3978204
Carlos Cueto <crcueto25@gmail.com>
2024-11-07 13:39:44 +0200

    fix cyclonedds
    topics visible between devices

--- wip.sh
+++ wip.sh
@@ -10,14 +10,15 @@ docker load -i ouster_lidar_image.tar
 
 docker-compose up -d
 
-docker-compose --progress plain -f ./docker/docker-compose.yml up
+docker compose --progress plain -f docker/docker-compose-desktop.yml build
 
 ros2 launch ouster_ros driver.launch.py \
 params_file:=src/ouster_config.yaml \
 viz:=false
 
-
-
+rsync -a -P \
+docker-images/ouster_lidar_image.tar \
+devuser@XXX.XXX.XXX.XXX:/home/devuser/repos/os1-lidar-jetson-tx1-humble/docker-images
 
 docker run -it \
 --privileged \
@@ -27,3 +28,5 @@ docker run -it \
 --env="QT_X11_NO_MITSHM=1" \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 osrf/ros:humble-desktop-full
+
+docker exec -it ouster_container bash
